190a0beb1fb82bb0547444f7c9eb708831ccd943
evelinymuniz <evelinymuniz@gamil.com>
2023-04-13 15:28:14 -0300

    start project

---
+++ src/main/java/br/com/udemy/webfluxudemy/WebfluxUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.webfluxudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/udemy/webfluxudemy/WebfluxUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.webfluxudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
