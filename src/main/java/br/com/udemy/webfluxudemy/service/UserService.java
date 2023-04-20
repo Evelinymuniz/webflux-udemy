0120d3f0118e8299182c9ee2377bc05e77ea6e9c
evelinymuniz <evelinymuniz@gamil.com>
2023-04-19 21:51:10 -0300

    add impl to delete user contract and handle not found

--- src/main/java/br/com/udemy/webfluxudemy/service/UserService.java
+++ src/main/java/br/com/udemy/webfluxudemy/service/UserService.java
@@ -24,9 +24,7 @@ public class UserService {
         return repository.save(mapper.toEntity(request));
     }
     public Mono<User> findById(final String id){
-        return repository.findById(id).switchIfEmpty(Mono.error(new ObjectNotFoundException(
-                format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
-        )));
+        return handleNotFoundTMono(repository.findById(id),id);
     }
 
     public Flux<User> findAll(){
@@ -37,6 +35,17 @@ public class UserService {
         return findById(id).map(entity -> mapper.toEntity(request, entity))
                 .flatMap(repository::save);
     }
+
+    public Mono<User> delete(final String id){
+        return handleNotFoundTMono(repository.findAndRemove(id),id);
+    }
+
+        private <T> Mono <T> handleNotFoundTMono(Mono<T> mono, String id){
+        return mono.switchIfEmpty(Mono.error(new ObjectNotFoundException(
+                    format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
+            )));
+
+        }
 
 
 
