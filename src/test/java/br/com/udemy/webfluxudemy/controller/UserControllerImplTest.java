865aa706cf8c8c5231a066bcfe6a6be3f8c7716a
evelinymuniz <evelinymuniz@gamil.com>
2023-04-25 17:07:58 -0300

    add refactor to attributes from test class

--- src/test/java/br/com/udemy/webfluxudemy/controller/UserControllerImplTest.java
+++ src/test/java/br/com/udemy/webfluxudemy/controller/UserControllerImplTest.java
@@ -9,20 +9,20 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
+
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -33,6 +33,10 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @SpringBootTest
 @AutoConfigureWebTestClient // fazer teste de integracao batendo no endpoint
 class UserControllerImplTest {
+    public static final String ID = "123456";
+    public static final String NAME = "Eveliny";
+    public static final String EMAIL = "eveliny01@gmail.com";
+    public static final String PASSWORD = "123";
     @Autowired
     private WebTestClient webTestClient;
     @MockBean
@@ -40,12 +44,12 @@ class UserControllerImplTest {
     @MockBean
     private UserMapper mapper;
     @MockBean
-    private MongoClient mongoClient;// conexao
+    private MongoClient mongoClient; // conexao
     @Test
     @DisplayName("Test endpoint save with success") //
     void testSaveWithSuccess() {
 
-        UserRequest request = new UserRequest("Eveliny", "eveliny01@gmail.com","123");
+        UserRequest request = new UserRequest(NAME, EMAIL,PASSWORD);
         Mockito.when(service.save(any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
 
         webTestClient.post().uri("/users")
@@ -58,7 +62,7 @@ class UserControllerImplTest {
     @DisplayName("Test endpoint save with bad request")
     void testSaveWithBadRequest() {
 
-        UserRequest request = new UserRequest(" Eveliny", "eveliny01@gmail.com","123");
+        UserRequest request = new UserRequest(NAME.concat(" "), EMAIL,PASSWORD);
 
         webTestClient.post().uri("/users")
                 .contentType(APPLICATION_JSON)
@@ -78,21 +82,21 @@ class UserControllerImplTest {
     @Test
     @DisplayName("Test find by id endpoint with success")
     void testfindByIdWithSuccess() {
-        final var id = "123456";
-        final var userResponse = new UserResponse(id,"Eveliny", "eveliny01@gmail.com","123");
+
+        final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
 
         when(service.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
         when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
 
-        webTestClient.get().uri("/users/"+ id)
+        webTestClient.get().uri("/users/"+ "123456")
                 .accept(APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .jsonPath("$.id").isEqualTo(id)
-                .jsonPath("$.name").isEqualTo("Eveliny")
-                .jsonPath("$.email").isEqualTo("eveliny01@gmail.com")
-                .jsonPath("$.password").isEqualTo("123");
+                .jsonPath("$.id").isEqualTo(ID)
+                .jsonPath("$.name").isEqualTo(NAME)
+                .jsonPath("$.email").isEqualTo(EMAIL)
+                .jsonPath("$.password").isEqualTo(PASSWORD);
 
 
     }
